713bb54d8f23f4dd9f7f439a3b28d033c230e80e
Andrea Borghi <andrea.borghi@camptocamp.com>
2019-11-18 16:44:59 +0100

    works also with PG

--- sqlalchemylogger/handlers.py
+++ sqlalchemylogger/handlers.py
@@ -1,17 +1,21 @@
 import logging
 import traceback
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Log, Base
+from .models import createLogClass, Base
 from sqlalchemy.exc import OperationalError, InvalidRequestError
+from sqlalchemy_utils import database_exists, create_database
 import threading, queue, time
 from .filters import ContainsExpression, DoesNotContainExpression
 
 
+module_logs = logging.getLogger(__name__)
+
 class SQLAlchemyHandler(logging.Handler):
     
     MAX_NB_LOGS = 100
-    MAX_TIMEOUT = 5
+    MAX_TIMEOUT = 1
 
     def __init__(self, sqlalchemyUrl, 
               doesNotContainExpression = None,
@@ -19,6 +23,9 @@ class SQLAlchemyHandler(logging.Handler):
         super().__init__()
         # initialize DB session
         self.engine = create_engine(sqlalchemyUrl['url'])
+        self.Log = createLogClass(
+                  tablename = sqlalchemyUrl.get('tablename', 'logs'),
+                  tableargs = sqlalchemyUrl.get('tableargs', None))
         Base.metadata.bind = self.engine
         DBSession = sessionmaker(bind=self.engine)
         self.session = DBSession()
@@ -56,7 +63,6 @@ class SQLAlchemyHandler(logging.Handler):
 
 
     def _writeLogs(self,logs):
-
        try:
            self.session.bulk_save_objects(logs)
            self.session.commit()
@@ -66,15 +72,23 @@ class SQLAlchemyHandler(logging.Handler):
                self.session.rollback()
                self.session.bulk_save_objects(logs)
                self.session.commit()
-           except:
+           except Exception as e:
                 # if we really cannot commit the log to DB, do not lock the
                 # thread and do not crash the application
+                module_logs.critical(e)
                 pass
        finally:
-            self.session.expunge_all()
+           self.session.expunge_all()
 
 
     def create_db(self):
+        if not database_exists(self.engine.url):
+            create_database(self.engine.url)
+        #FIXME: we should not access directly the private __table_args__
+        # variable, but add an accessor method in models.Log class
+        if type(self.Log.__table_args__) is not type(None) and self.Log.__table_args__.get('schema', None):
+            if not self.engine.dialect.has_schema(self.engine,self.Log.__table_args__['schema']):
+                self.engine.execute(sqlalchemy.schema.CreateSchema(self.Log.__table_args__['schema']))
         Base.metadata.create_all(self.engine)
 
 
@@ -83,7 +97,7 @@ class SQLAlchemyHandler(logging.Handler):
         exc = record.__dict__['exc_info']
         if exc:
             trace = traceback.format_exc()
-        log = Log(
+        log = self.Log(
             logger=record.__dict__['name'],
             level=record.__dict__['levelname'],
             trace=trace,
