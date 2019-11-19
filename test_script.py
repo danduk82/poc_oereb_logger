fb6432bc44775d9422afc98beed66a664e68d405
Andrea Borghi <andrea.borghi@camptocamp.com>
2019-11-19 16:31:09 +0100

    cleanup example code

--- test_script.py
+++ test_script.py
@@ -1,41 +1,16 @@
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import logging 
-import datetime
+import logging
+import time
 from sqlalchemylogger.handlers import SQLAlchemyHandler
-
-from sqlalchemylogger.models import Base, Log
 
 
 if __name__ == '__main__':
-    # simple logging config
     logging.basicConfig(
-         format='%(asctime)s : %(name)s : %(levelname)s : %(message)s',
-        level=logging.INFO,
+        format='%(asctime)s : %(name)s : %(levelname)s : %(message)s',
+        level=logging.DEBUG,
     )
     logger = logging.getLogger(__name__)
-    logger_db_engine = 'sqlite:///logger_db.sqlite3'
+    logger_db_engine = {'url':'sqlite:///logger_db.sqlite3'}
     
-    mylogger = SQLAlchemyHandler(logger_db_engine)
-    logger.addHandler(mylogger)
+    logger.addHandler(SQLAlchemyHandler(logger_db_engine))
     logger.info('bla')
-#
-#    oereb_logger = OEREBHttpLogger(session)
-#
-#    oereb_logger.setLevel = logging.DEBUG
-#    logger.addHandler(oereb_logger)
-#    log_adapter = logging.LoggerAdapter(logger,
-#                                        {'status':302,
-#                                         'flavour':'blabla',
-#                                         'requested_format': 'xml'})
-#    log_adapter.debug('marche pas')
-#    
-#
-#    log_adapter = logging.LoggerAdapter(logger,
-#                                        {'status':404,
-#                                         'flavour':'blabla',
-#                                         'requested_format': 'json',
-#                                         'service_type': 'GetCapabilities'})
-#    import pdb; pdb.set_trace()
-#    log_adapter.info('marche pas de nouveau')
+    time.sleep(10)
